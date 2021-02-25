fa93d73c66bc77703f6cfac0dd626d81b464676f
marco sartori <marcosartori98@yahoo.com>
2021-02-25 16:19:50 +0100

    modifica if

--- PHP Snack 2/index.php
+++ PHP Snack 2/index.php
@@ -7,18 +7,34 @@ Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”.
 <?php
 
 $nome = $_GET['nome'];
-$cognome = $_GET['cognome'];
+$mail = $_GET['mail'];
 $eta = $_GET['eta'];
+
+$accesso = '';
 
 $accesso = 'Inserisci un nome con più di 3 caratteri';
 
-if (strlen($nome) >3) {
-  $accesso = 'Inserisci un email corretta';
-
-  elseif (condition) {
-    // code...
-  }
+if (strlen($nome) < 3 || !strpos($mail, '@') || !strpos($mail, '.') || !isnumeric($eta)) {
+  $accesso = 'Accesso negato';
+}
+else {
+  $accesso = 'Accesso riuscito';
 }
 
 
  ?>
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <title>Document</title>
+ </head>
+ <body>
+
+   <h1> <?php echo $accesso; ?> </h1>
+
+ </body>
+ </html>
